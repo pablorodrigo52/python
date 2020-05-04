14f8cada59b0335cd664b206802fff92951932eb
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-05-04 00:07:16 -0300

    calculo desvio padrão sem o statistics

--- simple_math_operations.py
+++ simple_math_operations.py
@@ -5,10 +5,13 @@ numbers = [54.54, 54.545, 354, 332, 784, 154, 4.87, 0.45, -457]
 average = 142.38
 summation = 0
 
-for i in numbers:
+#average
+for i in numbers: 
     summation = summation + i
 average = summation / len(numbers)
 summation = 0
+
+#DP
 for i in numbers:
     summation = summation + ((i - average)**2)
 summation = summation / (len(numbers) - 1)
