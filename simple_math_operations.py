e051f9a67a901e779f2f91f8319aef6cb6ba8097
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-05-03 23:22:12 -0300

    Calculo do desvio padrão

--- simple_math_operations.py
+++ simple_math_operations.py
@@ -1,14 +1,18 @@
 import math
 import statistics
+
 numbers = [54.54, 54.545, 354, 332, 784, 154, 4.87, 0.45, -457]
 average = 142.38
+summation = 0
 
-sum = 0
-
-print(numbers)
 for i in numbers:
-    sum = sum + ((i - average)**2)
-sum = sum / (len(numbers) - 1)
+    summation = summation + i
+average = summation / len(numbers)
+summation = 0
+for i in numbers:
+    summation = summation + ((i - average)**2)
+summation = summation / (len(numbers) - 1)
 
-print('Desvio padrão calculado: ', math.sqrt(sum))
+print('Média: ', average)
+print('Desvio padrão calculado: ', math.sqrt(summation))
 print('Desvio padrão da lib statistics: ',statistics.stdev(numbers))
